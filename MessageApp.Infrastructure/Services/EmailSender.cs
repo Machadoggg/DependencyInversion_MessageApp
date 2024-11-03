05310a12f444ebdca760e16da04168f61847dcf0
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-11-02 21:58:16 -0500

    Implement IMessageSender with diferent strategies of send message

--- MessageApp.Infrastructure/Services/EmailSender.cs
+++ MessageApp.Infrastructure/Services/EmailSender.cs
@@ -1,7 +1,22 @@
-﻿namespace MessageApp.Infrastructure.Services
-{
-    public class EmailSender
-    {
+﻿using MessageApp.Core.Services;
 
+namespace MessageApp.Infrastructure.Services
+{
+    public class EmailSender : IMessageSender
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine($"Email sent: {message}");
+        }
     }
+
+    public class SmsSender : IMessageSender
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine($"Sms sent: {message}");
+        }
+    }
+
+
 }
