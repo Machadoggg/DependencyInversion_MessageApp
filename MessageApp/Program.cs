1e69c3a8993fc88ec45cc3d56014a43c48aea762
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-11-02 22:21:29 -0500

    Configure services

--- MessageApp/Program.cs
+++ MessageApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using MessageApp.Core.Services;
+using MessageApp.Infrastructure.Services;
+
+
+// Configuración de inyección de dependencias
+var serviceProvider = new ServiceCollection()
+    .AddTransient<IMessageSender, EmailSender>() // Cambia aquí para probar SmsSender
+    .BuildServiceProvider();
+
+var serviceProvider2 = new ServiceCollection()
+    .AddTransient<IMessageSender, SmsSender>()
+    .BuildServiceProvider();
+
+// Resolviendo la dependencia EmailSender
+var messageSender = serviceProvider.GetService<IMessageSender>();
+messageSender!.SendMessage("Hola, este es un mensaje de prueba!");
+
+// Resolviendo la dependencia SmsSender
+var messageSender2 = serviceProvider2.GetService<IMessageSender>();
+messageSender2!.SendMessage("Hola, este es un mensaje de prueba!");
+
+Console.WriteLine();
